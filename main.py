93832c410d9968e8ef5f3516196ccc76ae1b3ca5
enderwing <enderwing1@gmail.com>
2022-12-02 12:36:48 -0500

    minor changes
    - implemented mini-batch gradient decent for better performance tuning
    - qol code changes

--- main.py
+++ main.py
@@ -7,19 +7,22 @@ import time as t
 import matplotlib.pyplot as plt
 import numpy as np
 
+xrange = 50
+yrange = 50
+radius = 10
 
 def generate_data(amount):
 	data = []
 	for i in range(amount):
-		point = [r.random() * 10 - 5, r.random() * 10 - 5]
-		check = 1 if m.sqrt(point[0]**2 + point[1]**2) < 2.52 else 0
-		# check = 1 if point[1] < 2.5 else 0
+		point = [r.random() * xrange - xrange/2, r.random() * yrange - yrange/2]
+		# check = 1 if m.sqrt(point[0]**2 + point[1]**2) < radius else 0
+		check = 1 if point[1] < radius else 0
 		data.append(DataPoint(point, [check, 0 if check else 1]))
 	return data
 
 def graph_data(data: list[DataPoint], nnet: NeuralNetwork):
 	theta = np.arange(0, np.pi*2, 0.01)
-	plt.plot(2.52 * np.cos(theta), 2.52 * np.sin(theta))
+	plt.plot(radius * np.cos(theta), radius * np.sin(theta))
 	for i in range(len(data)):
 		outcome = nnet.classify(data[i])
 		if data[i].evs[0] == 1:
@@ -39,7 +42,8 @@ def graph_data(data: list[DataPoint], nnet: NeuralNetwork):
 def main():
 	nnet = NeuralNetwork([2, 3, 2])
 
-	numData = 50
+	numData = 200
+	sampleSize = 25
 	data = generate_data(numData)
 	while True:
 		print(nnet.cost_average(data))
@@ -49,8 +53,7 @@ def main():
 			print(layer.biases)
 		graph_data(data, nnet)
 		for i in range(1000):
-			nnet.learn(data, 0.15)
-
+			nnet.learn(r.sample(data, sampleSize), 0.15)
 
 
 if __name__ == '__main__':
